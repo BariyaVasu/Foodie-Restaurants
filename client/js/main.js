7b850820c2b4cd8f33574ae989bc653d1372034b
Harsh <chundadiyaharsh@gmail.com>
2025-11-23 19:09:51 +0530

    add swiper

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,11 @@
+ var swiper = new Swiper(".mySwiper", {
+    loop:true,
+      navigation: {
+        nextEl: "#next",
+        prevEl: "#prev",
+      },
+});
+
 const cartIcon = document.querySelector('.cart-icon');
 const cartTab = document.querySelector('.cart-tab');
 const closeBtn = document.querySelector('.close-btn');
